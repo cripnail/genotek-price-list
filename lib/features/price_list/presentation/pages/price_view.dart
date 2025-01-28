b360190842bc820d2c3af9edfd4538a5da334113
nsharipov <nikorurk@icloud.com>
2025-01-28 18:31:17 +0500

    chore: polishing

--- lib/features/price_list/presentation/pages/price_view.dart
+++ lib/features/price_list/presentation/pages/price_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genotek/core/constants/app_text_styles.dart';
+import 'package:genotek/core/constants/sizes.dart';
 import 'package:genotek/core/constants/strings.dart';
 import 'package:genotek/features/price_list/presentation/bloc/price_bloc.dart';
 import 'package:genotek/features/price_list/presentation/bloc/price_event.dart';
@@ -19,7 +20,7 @@ class PriceView extends StatelessWidget {
       body: BlocBuilder<PriceBloc, PriceState>(
         builder: (context, state) {
           return Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: const EdgeInsets.all(Sizes.contentPadding),
             child: Column(
               children: [
                 Text(
